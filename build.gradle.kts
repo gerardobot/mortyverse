c442da26a5b763bdef24f9dec3afff37d150be10
Gerardo Gomez Puerto <ggomez@atsistemas.com>
2026-03-14 01:03:11 +0100

    add ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,15 @@
 plugins {
     alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.ktlint)
+}
+
+ktlint {
+    android.set(true)
+    outputToConsole.set(true)
+
+    filter {
+        exclude("**/build/**")
+        exclude("**/generated/**")
+        include("**/*.kt")
+    }
 }
