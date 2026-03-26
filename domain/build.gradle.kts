496ba7ee1b1a53322495d57bd676a502bec63833
Gerardo Gomez Puerto <ggomez@atsistemas.com>
2026-03-27 00:29:05 +0100

    remove unused test dependencies

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -4,9 +4,4 @@ plugins {
 
 dependencies {
     implementation(libs.kotlinx.coroutines)
-
-    testImplementation(libs.test.kotlin)
-    testImplementation(libs.test.kotlinx.coroutines)
-    testImplementation(libs.test.junit)
-    testImplementation(libs.test.mockk)
 }
